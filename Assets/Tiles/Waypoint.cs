631a33528ec81d9541ac3066df20580db6c330fa
ub3rdevice <brigada1111@mail.ru>
2024-09-30 23:27:49 +0400

    implemented onMouseDown action & validation for correct tiles to build

--- Assets/Tiles/Waypoint.cs
+++ Assets/Tiles/Waypoint.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] bool isValidForPlacement;
+   void OnMouseDown() 
+   {
+        if(isValidForPlacement)
+        {
+            Debug.Log(transform.name);
+        }
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   }
 }

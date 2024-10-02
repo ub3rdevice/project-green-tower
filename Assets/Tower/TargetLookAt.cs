03ab8f7455029647485318e51571897d2db717a0
ub3rdevice <brigada1111@mail.ru>
2024-10-02 19:43:28 +0400

    implemented findClosestTarget

--- Assets/Tower/TargetLookAt.cs
+++ Assets/Tower/TargetLookAt.cs
@@ -6,26 +6,53 @@ using UnityEngine;
 public class TargetLookAt : MonoBehaviour
 {
     Transform target;
-
-    void Start()
-    {
-        FindAnEnemy();
-    }
-
-   
+    [SerializeField] float range = 15f;
+    [SerializeField] ParticleSystem projectileParticles;
 
     void Update()
     {
+        FindClosestTarget();
         TakeAim();
     }
 
     void TakeAim()
     {
         transform.LookAt(target);
+
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+        if (range > distanceToTarget)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+        
     }
 
-    void FindAnEnemy()
+    void Attack(bool isActive)
     {
-        target = FindObjectOfType<EnemyMover>().transform;
+        var emissionSetting = projectileParticles.emission;
+        emissionSetting.enabled = isActive;
+    }
+
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach(Enemy enemy in enemies) //not the best way in case of lots of enemies
+        {
+            float distanceToTarget = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if(distanceToTarget < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = distanceToTarget;
+            }
+        }
+        target = closestTarget;
     }
 }

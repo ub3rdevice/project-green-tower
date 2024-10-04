332d7cd1d7df16ad8852ecbe95cc8a59ae629a83
ub3rdevice <brigada1111@mail.ru>
2024-10-04 22:28:21 +0400

    added dictionary for coordinates in grid

--- Assets/Pathfinding/GridManager.cs
+++ Assets/Pathfinding/GridManager.cs
@@ -1,14 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] Node node;
-
-    void Start()
+    [SerializeField] Vector2Int gridSize; //vector2int = grid ref with x axis and y axis
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
+    void Awake()
     {
-        Debug.Log(node.coords);
-        Debug.Log(node.isNavigable);
+        PopulateGrid();
+    }
+
+    void PopulateGrid()
+    {
+        for(int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                Vector2Int coords = new Vector2Int(x,y);
+                grid.Add(coords, new Node(coords, true));
+                Debug.Log(grid[coords].coords + " = " + grid[coords].isNavigable);
+            }
+        }
     }
 }
